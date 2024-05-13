ceeb883eaa7f635daaf1d9ec9e8c5e9bdee8c0a7
Mironova Lyudmila <mirlual@yandex.ru>
2024-05-13 17:36:00 +0400

    Upd str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def upper_case(s):
+    '''
+    принимает на вход строку и возвращает ее со всеми заглавными буквами
+    '''
     return s.title()
+
+
+def upper_first_letter(s):
+    return s.capitalize()
